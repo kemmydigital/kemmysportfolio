fd268fbc0b4bce4b68d1f296fb3c55b7013383d5
kemmydigital <emmanuel16menyah@gmail.com>
2025-04-08 11:40:51 +0000

    `Update index.html, package.json and vite.config.ts for local development and Netlify deployment`

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    historyApiFallback: true,
+  }
 });
